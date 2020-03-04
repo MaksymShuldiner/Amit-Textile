fea0ac0b981916a48f94c2d2878f40e262ae06ea
Шульдинер Максим <max.shuldiner777@gmail.com>
2020-03-04 20:42:13 +0200

    жопа

--- Models/UserModel.cs
+++ Models/UserModel.cs
@@ -5,17 +5,17 @@ namespace AmitTextile.Models
 {
     public class UserModel
     {
-        [Required]
+        [Required(ErrorMessage = "Вы должны ввести ваш адрес")]
         [DataType(DataType.EmailAddress)]
         [EmailAddress(ErrorMessage = "Некорректный адрес")]
         public string Email { get; set; }
         
         public string Fio { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Пароль не может быть пустым")]
         [DataType(DataType.Password)]
         [StringLength(20, MinimumLength = 6, ErrorMessage = "Длина пароля должна быть от 6 до 20 символов")]
         public string Password { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Вы должны подтвердить пароль")]
         [Compare("Password", ErrorMessage = "Пароли не совпадают")]
         [DataType(DataType.Password)]
         public string PasswordConfirm { get; set; }
