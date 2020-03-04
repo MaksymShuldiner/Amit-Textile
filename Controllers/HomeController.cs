d0596e56fb49810d450a48d8a6f1d746ba37c9e8
Шульдинер Максим <max.shuldiner777@gmail.com>
2020-03-04 11:53:42 +0200

    d

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using AmitTextile.Domain;
 using AmitTextile.Domain.Context;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -14,14 +15,15 @@ namespace AmitTextile.Controllers
 {
     public class HomeController : Controller
     {
-        
-        public HomeController()
+        public AmitDbContext _context;
+        public HomeController(AmitDbContext context)
         {
-
+            _context = context;
         }
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            List<Category> Categories = await _context.Categories.ToListAsync();
+            return View(Categories);
         }
     }
 }
