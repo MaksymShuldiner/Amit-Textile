d4ec8022a38237e603bd1ef4ae7bd26804516934
Шульдинер Максим <max.shuldiner777@gmail.com>
2020-03-05 20:40:42 +0200

    erfgerf

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using AmitTextile.Domain;
 using AmitTextile.Domain.Context;
@@ -23,8 +24,20 @@ namespace AmitTextile.Controllers
         public async Task<IActionResult> Index()
         {
             List<Category> Categories = await _context.Categories.ToListAsync();
-
             return View(Categories);
+        }
+        [HttpGet]
+        public async Task<IActionResult> ShowCategory(string CatId, int page = 1)
+        {
+            int count = _context.Categories.Find(CatId).TextilesOfThisCategory.Count();
+            int textilesForPage = 9;
+            List<Textile> Textiles = _context.Categories.Find(CatId).TextilesOfThisCategory
+                .Skip(textilesForPage * (page - 1)).Take(textilesForPage).ToList();
+            List<ChildCategory> childCategories = _context.Categories.Find(CatId).ChildCategories.ToList();
+            PageViewModel pageViewModel = new PageViewModel(count, page, textilesForPage);
+            CategoriesViewModel model = new CategoriesViewModel()
+                {PageViewModel = pageViewModel, childCategories = childCategories, Textiles = Textiles};
+            return View(model);
         }
     }
 }
---
+++ Models/CategoriesViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using AmitTextile.Domain;
+
+namespace AmitTextile.Models
+{
+    public class CategoriesViewModel
+    {
+        public List<ChildCategory> childCategories { get; set; }
+
+        public List<Textile> Textiles { get; set; }
+
+        public PageViewModel PageViewModel { get; set; }
+
+    }
+}
