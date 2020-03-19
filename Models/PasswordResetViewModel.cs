2c806b2a388a950672517959dfd47b289e44a023
Шульдинер Максим <max.shuldiner777@gmail.com>
2020-03-19 16:18:59 +0200

    67

---
+++ Models/PasswordResetViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AmitTextile.Models
+{
+    public class PasswordResetViewModel
+    {
+        [Required(ErrorMessage = "Пароль не может быть пустым")]
+        [DataType(DataType.Password)]
+        [StringLength(20, MinimumLength = 6, ErrorMessage = "Длина пароля должна быть от 6 до 20 символов")]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "Вы должны подтвердить пароль")]
+        [Compare("Password", ErrorMessage = "Пароли не совпадают")]
+        [DataType(DataType.Password)]
+        public string PasswordConfirm { get; set; }
+    }
+}
